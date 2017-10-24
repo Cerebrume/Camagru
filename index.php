1d1b890a97f5646f5d70d2530fd72d5d239c4993
DmitryKazanovsky <dkazanov@student.unit.ua>
2017-10-24 17:30:51 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Camagru by dkazanov</title>
+</head>
+<body>
+	<div class="camera_canvas">
+		<video id="video" width="640" height="480" autoplay></video>
+		<button id="snap">Snap Photo</button>
+		<canvas id="canvas" width="640" height="480"></canvas>
+	</div>
+
+	<script src="assets/app.js"></script>
+</body>
+</html>
