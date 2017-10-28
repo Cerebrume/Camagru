9d12d39537a40944cb0e3c5adfbd72ae13186c32
Dmitro Kazanovsky <dkazanov@e1r1p17.unit.ua>
2017-10-28 16:37:32 +0300

    Create db setuping

--- config/database.php
+++ config/database.php
@@ -2,22 +2,4 @@
 	$DB_DSN = "mysql:host=localhost";
 	$DB_USER = "root";
 	$DB_PASSWORD = "3337815";
-
- 	 try {
-		$db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
-		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "CREATE DATABASE IF NOT EXISTS CAMAGRU;";
-		$sql .= "use CAMAGRU;";
-		$sql .= ("CREATE TABLE IF NOT EXISTS USERS (
-			id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-			login VARCHAR(30) NOT NULL,
-			email VARCHAR(50),
-			reg_date TIMESTAMP
-		);");
-		$db->exec($sql);
-		echo "SUCCESS";
-	} catch (PDOException $e) {
-		echo 'Connection failed: ' . $e->getMessage();
-	}
-	$db = NULL;
 ?>
