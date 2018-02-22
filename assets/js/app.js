f54e4648dba2f8b01feeff8f7103b6ff80312030
Cerebrume <dimakazanovsky@gmail.com>
2018-02-22 09:54:18 +0200

    add post page changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,49 @@ var ctx = canvas.getContext('2d');
 var localMediaStream = null;
 var img;
 var desc = document.getElementById('desc');
+var comment = document.getElementById('preview-bg__comment');
+
+var preview = document.getElementById('preview-bg');
+var preview__container = document.getElementById('preview-bg__container');
+
+function convertURIToImageData(URI) {
+	return new Promise(function(resolve, reject) {
+		if (URI == null) return reject();
+	
+		var image = new Image();
+		image.addEventListener('load', function() {
+		ctx.width = image.width;
+		canvas.height = image.height;
+		ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+		resolve();
+	  }, false);
+	  image.src = URI;
+	  preview__container.appendChild(image);
+	});
+  }
+
+function create_preview (img_src) {
+	preview.style.display = 'block';
+	convertURIToImageData(img_src)
+	.then(response => {
+		console.log(response);
+	});
+
+	var comment = document.createElement('div');
+	comment.classList.add('preview-bg__comment');
+	var btn_send = document.createElement('button');
+	btn_send.className = 'preview-bg__btn-send';
+	var textElem = document.createTextNode('Send');
+	btn_send.appendChild(textElem);
+	var btn_close = document.createElement('button');
+	btn_close.className = 'preview-bg__btn-close';
+	var textElem1 = document.createTextNode('Close');
+	btn_close.appendChild(textElem1);
+	preview__container.appendChild(comment);
+	preview__container.appendChild(btn_send);
+	preview__container.appendChild(btn_close);
+
+}
 
 function sendPic(img) {
 	var http = new XMLHttpRequest();
@@ -26,22 +69,21 @@ function sendPic(img) {
 	}
 	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	http.send("submit_img=true" + "&desc=" + desc + "&img=" + encodeURIComponent(img));
-	alert("submit_img=true" + "&desc=" + desc + "&img=" + encodeURIComponent(img));
 }
 
 function snapshot() {
 	if (localMediaStream) {
-		ctx.drawImage(video, 0, 0, 640, 480);
+		ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 		img = convertCanvasToImage(canvas);
-		sendPic(img);
-		//alert(img.src);
+		//sendPic(img);
+		create_preview(img);
 
 	}
 
 }
 
 function convertCanvasToImage(canvas) {
-	var image;
+	let image;
 	image = canvas.toDataURL("image/png");
 	return image;
 }
@@ -56,6 +98,5 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 		localMediaStream = stream;
 	});
 }
-
 
 
--- views/posts/add.php
+++ views/posts/add.php
@@ -4,21 +4,25 @@
 		<div class="row">
 			<div class="col-6">
 				<div class="camera-canvas">
-					<video id="video" width="640" height="480" autoplay></video>
-					<canvas id="canvas" width="640" height="480"></canvas>
+					<video id="video" width="480" height="360" autoplay></video>
+					<canvas id="canvas" width="480" height="360"></canvas>
 				</div>
 			</div>
 			<div class="post-add col-6">
+				<h3 class="file-h3">CHOSE PHOTO FROM FOLDER:</h3>
 					<input class="post-add__file" type="file" name="file" id="file" accept=".png, .jpg, .jpeg">
-					<label for="desc">Comment:</label>
+					<h3 class="comment-h3">YOUR COMMENT:</h3>
 					<textarea class="post-add__comment" style="resize:none;" name="desc" id="desc" cols="20" rows="3" maxlength="70"></textarea>
 					<button class="post-add__snap" id="snap">Snap Photo</button>
 			</div>
-			
-			
 		</div>
 	</div>
 </div>
 
+<div id="preview-bg" class="preview-bg">
+	<div id="preview-bg__container" class="preview-bg__container">
+	</div>
+
+</div>
+
 <script src="<?php echo ROOT_URL; ?>assets/js/app.js"></script>
-<script src="<?php echo ROOT_URL; ?>assets/js/filters.js"></script>
